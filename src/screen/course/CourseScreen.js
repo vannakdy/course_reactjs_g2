51e44b262b91f15d242c69abfdc26f3fb8884120
vannakdy <dyvannak1997@gmail.com>
2022-03-24 12:42:01 +0700

    new init project reactjs

---
+++ src/screen/course/CourseScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CourseScreen = () => {
+
+    return (
+        <div>
+            <h1>AboutUs Screen</h1>
+        </div>
+    )
+}
+
+export default CourseScreen;
