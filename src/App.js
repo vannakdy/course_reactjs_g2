068a9071434f519080774b9f00bf2278acc41924
vannakdy <dyvannak1997@gmail.com>
2022-04-25 21:38:23 +0700

    request data from api

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import CourseScreen from "./screen/course/CourseScreen";
 import AboutScreen from "./screen/about/AboutScreen";
 import StudentScreen from "./screen/student/StudentScreen";
 import StudentPaymentScreen from "./screen/student_payment/StudentPaymentScreen";
+import LoginScreen from './screen/auth/LoginScreen';
 import Route404 from './screen/404/Route404';
 import {BrowserRouter,Routes,Route,Link} from 'react-router-dom';
 import Container from './screen/container/Container';
@@ -32,6 +33,7 @@ const App = () => {
           <Route path='/about' element={<AboutScreen/>} />
           <Route path='/student' element={<StudentScreen/>} />
           <Route path='/student_payment' element={<StudentPaymentScreen/>} />
+          <Route path="/login" element={<LoginScreen/>} />
           <Route path='*' element={<Route404/>} />
         </Routes>
       </Container>
---
+++ src/screen/auth/LoginScreen.js
@@ -0,0 +1,57 @@
+import React , {useState} from "react";
+import "./LoginScreen.css";
+import axios from "axios";
+import {useNavigate} from 'react-router-dom'
+
+const LoginScreen = () => {
+    const navigate = useNavigate();
+    const [username,setUsername] = useState("");
+    const [password,setPassword] = useState("");
+    const [loading , setLoading ] = useState(false);
+    const [message , setMessage ] = useState("");
+    const handleLogin = () => {
+        setLoading(true);
+        setMessage("");
+        axios({
+            method : "POST",
+            url : "https://nitc.cleverapps.io/api/auth/login",
+            data : {
+                "username" : username,
+                "password" : password
+            }
+        }).then(response=>{
+            setLoading(false);
+            var data = response.data;
+            if(data.message){
+                setMessage(data.message);
+            }else{
+                navigate("/")
+            }
+        })
+    }
+    return(
+        <div className="login_container">
+            <div className="login_form">
+                <h1>Login</h1>
+                <input
+                    value={username}
+                    className="input" 
+                    placeholder="Username"
+                    onChange={(event)=>setUsername(event.target.value)}
+                /> <br/>
+                <input
+                    value={password}
+                    type={"password"}
+                    className="input" 
+                    placeholder="Password"
+                    onChange={(event)=>setPassword(event.target.value)}
+                /> <br/>
+                {loading === true && <div style={{marginTop:10}}>Loading ...</div>}
+                {message !== "" && <div style={{marginTop:10,color:"red"}}>{message}</div>}
+                <button onClick={handleLogin} className="btn_login">Login</button>
+            </div>
+        </div>
+    )
+}
+
+export default LoginScreen;
