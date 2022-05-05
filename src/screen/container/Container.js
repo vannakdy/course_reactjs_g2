99c2a45e06399856cf611d00efefb7948b9cddd4
vannakdy <dyvannak1997@gmail.com>
2022-05-05 21:38:22 +0700

    test

--- src/screen/container/Container.js
+++ src/screen/container/Container.js
@@ -1,22 +1,144 @@
 import React  from "react";
 import {Link} from 'react-router-dom';
+import {
+    UserOutlined,
+    DownOutlined,
+    SettingOutlined, 
+    LogoutOutlined,
+    HomeFilled,
+    TeamOutlined,
+    SwitcherOutlined,
+    ImportOutlined
+    
+} from "@ant-design/icons";
+import {Dropdown,Menu,Space,Spin} from "antd";
 import LoadingCenter from "../../component/loading/LoadingCenter";
 const Container = (props) => {
     const is_login = localStorage.getItem("is_login");
+    const username = localStorage.getItem("username");
     const handleLogout = () => {
         localStorage.setItem("is_login","false");
         window.location.href = "/login"
     }
+    const menu = (
+        // <div style={{padding:10,backgroundColor:"gray"}}>
+        //     <div>Ddd</div>
+        //     <div>Ddd</div>
+        //     <div>Ddd</div>
+        //     <div>Ddd</div>
+        //     <div>Ddd</div>
+        //     <div>Ddd</div>
+        // </div>
+
+
+        <Menu
+            style={{width:150}}
+            items={[
+                {
+                    label : <Link to="/profile">Profile</Link>,
+                    icon : <UserOutlined />
+                },
+                {
+                    label : <Link to="/setting">Setting</Link>,
+                    icon : <SettingOutlined />,
+                    disabled : true
+                },
+                {
+                    label : <Link to="/login">Logout</Link>,
+                    icon : <LogoutOutlined />,
+                    danger : true
+                }
+            ]}  
+        />
+
+        // <Menu
+        //     items={[
+        //     {
+        //         label: (
+        //         <a target="_blank" rel="noopener noreferrer" href="https://www.antgroup.com">
+        //             1st menu item
+        //         </a>
+        //         ),
+        //     },
+        //     {
+        //         label: (
+        //         <a target="_blank" rel="noopener noreferrer" href="https://www.aliyun.com">
+        //             2nd menu item (disabled)
+        //         </a>
+        //         ),
+        //         icon: <SmileOutlined />,
+        //         disabled: true,
+        //     },
+        //     {
+        //         label: (
+        //         <a target="_blank" rel="noopener noreferrer" href="https://www.luohanacademy.com">
+        //             3rd menu item (disabled)
+        //         </a>
+        //         ),
+        //         disabled: true,
+        //     },
+        //     {
+        //         danger: true,
+        //         label: 'a danger item',
+        //     },
+        //     ]}
+        // />
+    );
+
     return(
         <div>
+            
             {is_login == "true" ?  
                 <div className='main_menu'>
-                    <Link className='item_menu' to="/">Home</Link>
-                    <Link className='item_menu' to="/course">Course</Link>
-                    <Link className='item_menu' to="/about">About</Link>
-                    <Link className='item_menu' to="/student">Student</Link>
-                    <Link className='item_menu' to="/student_payment">Student Payemt</Link>
-                    <Link className='item_menu' to="/login"  onClick={handleLogout} >Logout</Link>
+                    <div>
+                        <Link className='item_menu' to="/">
+                            <Space>
+                                <HomeFilled/>HOME
+                            </Space>
+                        </Link>
+                        <Link className='item_menu' to="/student">
+                            <Space>
+                                <TeamOutlined/>STUDENT
+                            </Space>
+                        </Link>
+                        <Link className='item_menu' to="/course">
+                            <Space>
+                                <SwitcherOutlined />COURSES
+                            </Space>
+                        </Link>
+                        <Link className='item_menu' to="/about">
+                            <Space>
+                                < ImportOutlined/>ABOUT
+                            </Space>
+                        </Link>
+                        <Link className='item_menu' to="/student_payment">
+                            <Space>
+                                < ImportOutlined/>STUDENT PAYMENT
+                            </Space>
+                        </Link>
+                        <Link className='item_menu' to="/student_payment">
+                            <Dropdown overlay={menu}>
+                                <a className="item_menu" onClick={e => e.preventDefault()}>
+                                <Space>
+                                    MORE<DownOutlined />
+                                </Space>
+                                </a>
+                            </Dropdown>
+                        </Link>
+                    </div>
+                    <div>
+                       
+                        <Dropdown overlay={menu}>
+                            <a className="item_menu" onClick={e => e.preventDefault()}>
+                            <Space>
+                                <UserOutlined/>
+                                {username.toUpperCase()}
+                                <DownOutlined />
+                            </Space>
+                            </a>
+                        </Dropdown>
+                    </div>
+                    
                 </div>
                 :
                 <div className='main_menu'>
@@ -24,7 +146,9 @@ const Container = (props) => {
                 </div>
             }
             <div style={{height:600,padding:30}}>
-                {props.children}
+                <Spin spinning={true}>
+                    {props.children}
+                </Spin>
             </div>
         </div>
     )
